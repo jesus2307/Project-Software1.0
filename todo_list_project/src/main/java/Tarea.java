0ac4ab623254f1c6eaa5c1dbebe5882baf435349
jesus <60348759+jesus2307@users.noreply.github.com>
2024-11-09 18:48:16 +0100

    Add files via upload

---
+++ todo_list_project/src/main/java/Tarea.java
@@ -0,0 +1,23 @@
+
+public class Tarea {
+    private String nombre;
+    private boolean completada;
+
+    public Tarea(String nombre) {
+        this.nombre = nombre;
+        this.completada = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void completar() {
+        this.completada = true;
+    }
+}
+
